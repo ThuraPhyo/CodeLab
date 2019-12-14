02242dd8033059d60dea4e6f0ae315ffbee2dbf1
Thura <thura.bgo.war.0502@gmail.com>
2019-12-14 23:12:14 +0630

    -add FireBase cloud messaging testing

--- app/src/main/java/com/mingalarinfotech/codelab/activities/MainActivity.kt
+++ app/src/main/java/com/mingalarinfotech/codelab/activities/MainActivity.kt
@@ -3,6 +3,10 @@ package com.mingalarinfotech.codelab.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.mingalarinfotech.codelab.R
 
 class MainActivity : BaseActivity() {
@@ -10,9 +14,25 @@ class MainActivity : BaseActivity() {
         fun newIntent(context : Context) : Intent {
             return Intent(context, MainActivity::class.java)
         }
+        private const val TAG = "FireBaseToken"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+
+                Log.d(TAG, token)
+            })
     }
 }
