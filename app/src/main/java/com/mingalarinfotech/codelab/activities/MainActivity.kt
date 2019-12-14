16c9780f3c0f3fa25a6680ef1e21822f1c1f3226
Thura <thura.bgo.war.0502@gmail.com>
2019-12-14 22:52:07 +0630

    -login with Google

--- app/src/main/java/com/mingalarinfotech/codelab/activities/MainActivity.kt
+++ app/src/main/java/com/mingalarinfotech/codelab/activities/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mingalarinfotech.codelab.activities
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.mingalarinfotech.codelab.R
 
 class MainActivity : BaseActivity() {
-
+    companion object{
+        fun newIntent(context : Context) : Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
